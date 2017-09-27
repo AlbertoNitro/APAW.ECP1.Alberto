c021e5e3c0b661178dd1fbacfe19b8754d951024
AlbertoNitro <albertojgm@yahoo.es>
2017-09-27 20:36:25 +0200

    Created DriverBuilderTest

--- src/main/java/builder/DriverBuilder.java
+++ src/main/java/builder/DriverBuilder.java
@@ -7,7 +7,7 @@ import entity.Level;
 public class DriverBuilder {
     private Driver driver;
 
-    public DriverBuilder(int id, Level level, Car car) {
+    public DriverBuilder(int id, Level level, Car car) { // Como la entidad que me ha tocado, tiene dos relaciones de agregacion decido que al crear el condcutor, es necesario que me pasen el nivel y el coche del conductor.
         this.driver = new Driver(id, level, car);
     }
 
@@ -16,8 +16,8 @@ public class DriverBuilder {
         return this;
     }
 
-    public DriverBuilder reference(long phone) {
-        this.driver.setPhone(phone);
+    public DriverBuilder reference(String reference) {
+        this.driver.setReference(reference);
         return this;
     }
 
