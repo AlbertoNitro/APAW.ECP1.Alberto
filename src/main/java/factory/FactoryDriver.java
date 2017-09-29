aee889e31774b854aa06d0f892bcb5f10686bebb
AlbertoNitro <albertojgm@yahoo.es>
2017-09-29 11:00:37 +0200

    Modify FactoryDriver, FactoryDriverTest

--- src/main/java/factory/FactoryDriver.java
+++ src/main/java/factory/FactoryDriver.java
@@ -1,5 +1,6 @@
 package factory;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,26 +8,46 @@ import entity.Driver;
 
 public class FactoryDriver {
     private Map<Integer, Driver> factory;
-    
-    public FactoryDriver() {
+
+    private static final FactoryDriver factoryDriver = new FactoryDriver();
+
+    private FactoryDriver() {
         this.factory = new HashMap<Integer, Driver>();
     }
-    
+
     public void almacenate(Driver driver) {
         Integer idDriver = new Integer(driver.getId());
-        if (this.factory.get(idDriver)==null) {
+        if (this.factory.get(idDriver) == null) {
             this.factory.put(idDriver, driver);
         } else {
             throw new UnsupportedOperationException("Conductor ya existente en la factoria con ese ID");
         }
     }
-    
+
     public boolean search(Driver driver) {
         Integer idDriver = new Integer(driver.getId());
         boolean located = false;
-        if (this.factory.get(idDriver)!=null) {
+        if (this.factory.get(idDriver) != null) {
             located = true;
         }
         return located;
+    }
+    
+    public void clear() {
+        this.factory.clear();
+    }
+    
+    @SuppressWarnings("unused")
+    public int size() {
+        Collection<Driver> values = this.factory.values();
+        int cont=0;
+        for (Driver driver: values) {
+            cont++;
+        }
+        return cont;
+    }
+
+    public static FactoryDriver getFactoryDriver() {
+        return factoryDriver;
     }
 }
