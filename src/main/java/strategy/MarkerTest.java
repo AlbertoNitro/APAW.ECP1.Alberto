1a9c95730c80e8fb6139bbeacfbbc80a96c3c23e
AlbertoNitro <albertojgm@yahoo.es>
2017-10-02 20:16:38 +0200

    Created *Strategy, Marker

---
+++ src/main/java/strategy/MarkerTest.java
@@ -0,0 +1,19 @@
+package strategy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarkerTest {
+
+    @Test
+    public void testMarker() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDoMarked() {
+        fail("Not yet implemented");
+    }
+
+}
